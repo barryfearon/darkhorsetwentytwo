3776ea154de9005c304e943cf2dc8c12668ccda9
Barry Fearon <barry.fearon@gamingmechanics.com>
2022-04-11 09:47:27 +0100

    mobile menu set up

--- app/src/components/MainLayout/Header.tsx
+++ app/src/components/MainLayout/Header.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Link from 'next/link'
 import { Col, Row, Nav } from 'react-bootstrap'
+import { NavigationItems } from './NavigationItems'
+import MobileNavigation from '../MainLayout/MobileNavigation'
 
 import styles from './Header.module.scss'
 
@@ -10,31 +12,26 @@ interface IHeaderProps {
 
 export const Header: React.FC<IHeaderProps> = ({ siteLogo }) => {
   return (
-    <Row className='justify-content-center'>
-      <Col sm={10} lg={2} className='pt-3 d-flex flex-column align-items-center'>
-        <div className='dhLogo'>
-          <a href='/' className='stretched-link btn-link'>
-            <img
-              src='https://res.cloudinary.com/dark-horse-music/image/upload/v1645472154/logos/logo-twenty-two.svg'
-              width='447'
-              height='373'
-              className='img-fluid'
-            />
-          </a>
+    <>
+      <MobileNavigation />
+      <Row className='justify-content-center'>
+        <Col sm={10} lg={2} className='pt-3 d-flex flex-column align-items-center'>
+          <div className='dhLogo'>
+            <a href='/' className='stretched-link btn-link'>
+              <img
+                src='https://res.cloudinary.com/dark-horse-music/image/upload/v1645472154/logos/logo-twenty-two.svg'
+                width='447'
+                height='373'
+                className='img-fluid'
+              />
+            </a>
+          </div>
+        </Col>
+        <div className={styles.MainNav}>
+          <NavigationItems />
         </div>
-      </Col>
-      <div className={styles.MainNav}>
-        <Link href='/'>
-          <a className='btn btn-outline-danger text-white mx-2'>Home Page</a>
-        </Link>
-        <Link href='/lineup'>
-          <a className='btn btn-outline-danger text-white mx-2'>Dark Star Barn</a>
-        </Link>
-        <Link href='/festival-facts'>
-          <a className='btn btn-outline-danger text-white mx-2'>Festival Facts</a>
-        </Link>
-      </div>
-    </Row>
+      </Row>
+    </>
   )
 }
 
